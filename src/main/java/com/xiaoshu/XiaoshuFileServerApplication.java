50586b75912113c687ff1bfbcf3ccc8aa7b32a83
贾亮亮 <deane163@126.com>
2021-02-09 11:19:07 +0800

    first commit

---
+++ src/main/java/com/xiaoshu/XiaoshuFileServerApplication.java
@@ -0,0 +1,25 @@
+package com.xiaoshu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 功能说明：
+ *
+ * @ com.xiaoshu
+ * <p>
+ * Original @Author: deane.jia-贾亮亮,@2021/2/9@11:08
+ * <p>
+ * Copyright (C)2012-@2021 深圳优必选科技 All rights reserved.
+ */
+@SpringBootApplication
+@Slf4j
+public class XiaoshuFileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoshuFileServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xiaoshu/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.xiaoshu.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 功能说明：
+ *
+ * @ com.xiaoshu.controller
+ * <p>
+ * Original @Author: deane.jia-贾亮亮,@2021/2/9@11:08
+ * <p>
+ * Copyright (C)2012-@2021 深圳优必选科技 All rights reserved.
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/main")
+public class MainController {
+
+}
